c6c959743b9c14a1ad0b3303449364276ac5c4e2
ViniciusDadalte <viniciusdadalte745@gmail.com>
2024-09-28 19:50:34 -0300

    Fiz a aba do suporte, o codigo js para ela, o css, e tambem fiz a pagina que vai ficar a inteligencia artificial, fazendo tambem o codigo para ela funcionar, faltando apenas o token, e fiz o css para ficar bonito

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-function toggleMenu() {
+function toggleMenu() 
+{
     const nav = document.querySelector('nav');
     nav.classList.toggle('active');
 }
+
+function abrirSuporte() 
+{
+    document.getElementById('abaSuporte').classList.add('ativa');
+}
+
+function fecharSuporte() 
+{
+    document.getElementById('abaSuporte').classList.remove('ativa');
+}
+
+// function expandirChatBot() 
+// {
+//     const chatBot = document.getElementById('chatBot');
+//     const suporteAba = document.getElementById('abaSuporte');
+
+//     if (chatBot.classList.contains('expandido')) 
+//     {
+//         chatBot.classList.remove('expandido');
+//         chatBot.style.height = 'auto'; // Retorna a altura para automática
+//     } 
+//     else 
+//     {
+//         chatBot.classList.add('expandido');
+//         chatBot.style.height = 'calc(100% - 40px)'; // Ajuste para cobrir toda a aba
+//     }
+// }
