497c2d43e742a1277904634bf6830f4fab98ce3d
ViniciusDadalte <viniciusdadalte745@gmail.com>
2024-09-28 23:47:02 -0300

    Fiz alguns ajustes nessesarios na aba de suporte, agora com uma pré-visualização do chatbot fazendo com que ele expanda quando clicarmos, e tambem fiz alguns ajustes ao todo

--- js/index.js
+++ js/index.js
@@ -14,27 +14,12 @@ function fecharSuporte()
     document.getElementById('abaSuporte').classList.remove('ativa');
 }
 
-function expandirChatBot() {
+function expandirChatBot() 
+{
     document.getElementById('chatbot-expanded').classList.add('active');
 }
 
-function recolherChatBot() {
+function recolherChatBot() 
+{
     document.getElementById('chatbot-expanded').classList.remove('active');
 }
-
-// function expandirChatBot() 
-// {
-//     const chatBot = document.getElementById('chatBot');
-//     const suporteAba = document.getElementById('abaSuporte');
-
-//     if (chatBot.classList.contains('expandido')) 
-//     {
-//         chatBot.classList.remove('expandido');
-//         chatBot.style.height = 'auto'; // Retorna a altura para automática
-//     } 
-//     else 
-//     {
-//         chatBot.classList.add('expandido');
-//         chatBot.style.height = 'calc(100% - 40px)'; // Ajuste para cobrir toda a aba
-//     }
-// }
